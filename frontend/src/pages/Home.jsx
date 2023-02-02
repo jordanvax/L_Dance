e332e7c17968c77862426af5602dc93f6f29d6d5
jordanvax <jordan.vaxelaire@hotmail.fr>
2023-02-02 14:31:30 +0100

    hotfix fichier home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,5 @@
-import Counter from "../components/Counter";
-import logo from "../assets/logo.svg";
-
 export default function Home() {
   return (
-    <h1 class="text-3xl font-bold underline text-blue-500">
-    Hello world!
-  </h1>
+    <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
   );
 }
